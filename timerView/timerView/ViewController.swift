caa976578f919176c45e70b1bae701fd840773cb
tsteinberg77 <31593558+tsteinberg77@users.noreply.github.com>
2017-12-02 19:15:19 -0500

    Add files via upload

---
+++ timerView/timerView/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  timerView
+//
+//  Created by SOM Media Center on 12/2/17.
+//  Copyright © 2017 SOM Media Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //FOR EACH LABEL:
+    @IBOutlet weak var worklbl: UILabel!
+    @IBOutlet weak var restlbl: UILabel!
+    
+    
+    //var time = 0
+    
+    //ONE OF THESE FOR WORK AND ONE FOR REST
+    //func action()
+    //{
+            //time -=1
+            //worklbl.text = string(time)
+    //}
+    
+    //func action()
+    //{
+            //time -=1
+            //restlbl.text = string(time)
+    //}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
